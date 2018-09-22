3e347cbbce5d2da5b8a12d38ae8e442d1abf6acb
root <root@ldap.aliyun.sz.deepcam.privsub.net>
2018-09-22 12:55:20 +0800

    新增: 磁盘发现规则以及IO指标

---
+++ scripts/disk_discover.py
@@ -0,0 +1,11 @@
+#!/bin/env python
+#coding:utf-8
+# 获取磁盘脚本
+import commands
+import json
+
+cmd="egrep '[shv]d[a-z]+$' /proc/partitions | awk '{print $4}'"
+result = commands.getoutput(cmd).split()
+
+disks = [ {'{#DISK_NAME}':disk} for disk in result ]
+print(json.dumps({'data':disks},indent=4,separators=(',',':')))
